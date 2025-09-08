edd22a83eca539f4e83db1a7b984b1967ed0b8de
Robiul Islam <robi.kushtia@gmail.com>
2025-09-08 12:37:11 +0600

    Cart section updated

--- script.js
+++ script.js
@@ -36,8 +36,8 @@ const setupButton = (plants) => {
                <p class="text-[14px] font-normal bg-[#dcfce7] rounded-full inline-block px-3 py-2">${plant.category}</p>
                <p class="p-2 text-[14px] font-normal"><span>৳${plant.price}</span></p>
             </div>
-            <button
-            class="bg-green-700 text-white px-5 py-2 w-full rounded-full text-[16px] font-medium cursor-pointer"
+            <button id="btn-add-to-cart"
+            class="btn-button bg-green-700 text-white px-5 py-2 w-full rounded-full text-[16px] font-medium cursor-pointer"
               >
             Add to Cart
               </button>
@@ -117,8 +117,8 @@ const displayPlants = (plants) => {
                <p class="text-[14px] font-normal bg-[#dcfce7] rounded-full inline-block px-3 py-2">${plant.category}</p>
                <p class="p-2 text-[14px] font-normal"><span>৳${plant.price}</span></p>
             </div>
-            <button
-            class="bg-green-700 text-white px-5 py-2 w-full rounded-full text-[16px] font-medium cursor-pointer"
+            <button id="btn-add-to-cart"
+            class="btn-button bg-green-700 text-white px-5 py-2 w-full rounded-full text-[16px] font-medium cursor-pointer"
               >
             Add to Cart
               </button>
@@ -128,3 +128,57 @@ const displayPlants = (plants) => {
     parentCardDiv.appendChild(card);
   });
 };
+
+// Cart Section =========================
+document.getElementById("parent-card").addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn-button")) {
+    handleAddToCart(e.target);
+  }
+});
+
+function handleAddToCart(button) {
+  let myCart = document.getElementById("my-cart");
+  const card = button.closest(".single-card");
+  const name = card.querySelector("p").innerText;
+  let cartPrice = Number(card.querySelector("span").innerText.replace("৳", ""));
+
+  let totalPrice = Number(document.getElementById("total-price").innerText);
+
+  let newCart = document.createElement("div");
+  newCart.innerHTML = `
+      
+      <div
+            class="single-cart flex justify-between px-3 py-2 bg-[#F0FDF4] rounded-lg"
+            >
+            <div class="cart-content">
+              <h2 class="text-[14px] font-semibold">${name}</h2>
+              <p class="text-[16px] font-normal">৳<span class="cart-price">${cartPrice}</span> x 1</p>
+            </div>
+            <div class="close">
+              <span id="btn-close" class="btn-close cursor-pointer">X</span>
+            </div>
+          </div>
+      `;
+
+  const totalDiv = myCart.querySelector(".total");
+
+  myCart.insertBefore(newCart, totalDiv);
+
+  // Total
+  let newTotal = totalPrice + cartPrice;
+  document.getElementById("total-price").innerText = newTotal;
+}
+
+// Cart item remove
+document.getElementById("my-cart").addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn-close")) {
+    const cartItem = e.target.closest(".single-cart");
+    const priceEl = cartItem.querySelector(".cart-price");
+    const price = Number(priceEl.innerText);
+
+    const totalPriceEl = document.getElementById("total-price");
+    totalPriceEl.innerText = Number(totalPriceEl.innerText) - price;
+
+    cartItem.remove();
+  }
+});
